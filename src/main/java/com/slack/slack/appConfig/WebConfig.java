b2a1249d7f36f4ee94bbed08816d881bb941a72b
donghyeon0725 <ehdgus5015@gmail.com>
2021-05-22 21:32:10 +0900

    git add setting to find static resource

--- src/main/java/com/slack/slack/appConfig/WebConfig.java
+++ src/main/java/com/slack/slack/appConfig/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,6 +21,7 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(htmlEscapingConverter());
@@ -30,4 +33,16 @@ public class WebConfig implements WebMvcConfigurer {
 
         return new MappingJackson2HttpMessageConverter(objectMapper);
     }
+
+    /* 정적 자원 매핑을 위해 필요한 설정 */
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
 }
