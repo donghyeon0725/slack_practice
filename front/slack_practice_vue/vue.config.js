5e5f4f04c64a48fe52f2b691335c9670a7b3645d
donghyeon0725 <ehdgus5015@gmail.com>
2021-07-22 12:46:58 +0900

    spring boot  + vue 연동 완료

--- front/slack_practice_vue/vue.config.js
+++ front/slack_practice_vue/vue.config.js
@@ -1,7 +1,8 @@
 'use strict'
 const path = require('path')
 module.exports = {
-  outputDir: path.resolve(__dirname, '../../src/main/resources/static'),
+  outputDir: path.resolve(__dirname, '../../src/main/resources/static/vue'),
+  indexPath: path.resolve(__dirname, '../../src/main/resources/templates/index.mustache'),
 
   devServer: {
     overlay: false, // 웹팩 데브에서 제공하는 에러 화면 나타내 주는 기능을 끄는 것
